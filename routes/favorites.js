51c70116c892a5a05b76e947b10fb0961f0afafd
Sara Keyser <yogasara@icloud.com>
2018-12-13 13:28:26 -0700

    built one get route with inner join for users and trails.

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const knex = require('../knex')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/favorites',(req, res) => {
+  // console.log(req.userCredentials);
+  return knex('favorites')
+    .innerJoin('users', 'favorites.users_id', 'users.id')
+    .innerJoin('trails', 'favorites.trails_id', 'trails.id')
+    .then((result) => {
+      res.status(200).send(result)
+    })
+})
 
-module.exports = router;
+router.get('/', ())
+// - Favorites
+//   - POST /api/favorites add a new favorite
+//   - GET /api/favorites Retrieve all favorites
+//   - GET /api/favorites/:id Retrieve a single favorite
+//   - DELETE /api/favorites/:id Delete an favorite
+//
+
+
+module.exports = router
