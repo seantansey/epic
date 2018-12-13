4d72b24c16878de46a1094f51a9933761eb9a766
Sara Keyser <yogasara@icloud.com>
2018-12-13 14:58:16 -0700

    trail routes complete, need auth

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 require('dotenv').config()
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
 var favoritesRouter = require('./routes/favorites')
+var trailsRouter = require('./routes/trails')
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/favorites', favoritesRouter)
+app.use('/trails', trailsRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/trails.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var router = express.Router()
+const knex = require('../knex')
+// - Trails
+
+router.get('/', (req, res, next)=>{
+  return knex('trails')
+  .then((trail)=>{
+    res.send(trail[0])
+  })
+  .catch((err) => {
+    next(err)
+  })
+})
+
+router.get('/:id', (req, res, next)=>{
+  return knex('trails')
+  .where('id',req.params.id)
+  .then((trail)=>{
+    res.send(trail[0])
+  })
+  .catch((err) => {
+    next(err)
+  })
+})
+
+module.exports = router
