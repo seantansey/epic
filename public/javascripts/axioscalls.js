8460972ee291973d0dd5d971a9990a5abf4f315b
Paul Mourer <paul.mourer@gmail.com>
2018-12-14 13:21:25 -0700

    trails axios calls, append to test.html added verify to favorites routes

--- public/javascripts/axioscalls.js
+++ public/javascripts/axioscalls.js
@@ -1,11 +1,37 @@
+//Users
+//
+//POST /api/signup Create a new user
+//POST /api/login User login
+//POST /api/logout logout user
+//GET /api/users/:id Retrieve user information
+//Favorites
+//
+//POST /api/favorites add a new favorite
+//GET /api/favorites Retrieve all favorites
+//GET /api/favorites/:id Retrieve a single favorite
+//DELETE /api/favorites/:id Delete an favorite
+//Trails
+//
+//GET /api/trails/:id get a single trail
+//GET /api/trails get all trails
 
 
 function getAllTrails () {
-  axios.get('/trails').then(data =>{
+  axios.get('/trails')
+  .then(data => {
+    putOnPage(data.data)
+  })
+}
+
+function getOneTrail (id) {
+  axios.get(`/trails/${id}`).then(data => {
     putOnPage(data.data)
   })
 }
 
 function putOnPage (data) {
-  document.querySelector('body').innerText = JSON.stringify(data)
+  console.log('hi');
+  let p = document.createElement('p')
+  p.innerText = JSON.stringify(data)
+  document.querySelector('body').appendChild(p)
 }
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
   getAllTrails()
+  getOneTrail(4)
 })
