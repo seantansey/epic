95df0c5626cd9913141cd9d4ce9adae4104a4ebe
Paul <paul.mourer@gmail.com>
2018-12-16 14:55:12 -0700

    axioscalls.js fixed

--- public/javascripts/axioscalls.js
+++ public/javascripts/axioscalls.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-//Users
-//
-//POST /api/signup Create a new user
-//POST /api/login User login
-//POST /api/logout logout user
-//GET /api/users/:id Retrieve user information
-//Favorites
-//
-//POST /api/favorites add a new favorite
-//GET /api/favorites Retrieve all favorites
-//GET /api/favorites/:id Retrieve a single favorite
-//DELETE /api/favorites/:id Delete an favorite
-//Trails
-//
-//GET /api/trails/:id get a single trail
-//GET /api/trails get all trails
-
-
-function getAllTrails () {
-  axios.get('/trails')
-  .then(data => {
-    console.log(data.data)
-=======
 // Trails
 
 
@@ -99,52 +75,13 @@ const getAFavorite = (id) => {
 const deleteFavorite = id => {
   axios.delete(`/favorites/${id}`)
   .then(data => {
->>>>>>> 75d57549ff9e6c350f70c3010b1a4b63da95ec17
     return data.data
   })
 }
 
-<<<<<<< HEAD
-function getOneTrail (id) {
-  axios.get(`/trails/${id}`).then(data => {
-    putOnPage(data.data)
-  })
-}
-
-function putOnPage (data) {
-  console.log('hi');
-=======
 
 const putOnPage = (data) => {
->>>>>>> 75d57549ff9e6c350f70c3010b1a4b63da95ec17
   let p = document.createElement('p')
   p.innerText = JSON.stringify(data)
   document.querySelector('body').appendChild(p)
 }
-<<<<<<< HEAD
-  // POST /api/signup Create a new user
-  const createNewUser = (newUser) => {
-    axios.post('/users', newUser)
-      .then(data => {
-        putOnPage(data.data)
-      })
-  }
-
-
-  // POST /api/login User login
-  const loginUser = (user) => {
-    axios.post('/token', user)
-      .then(data => {
-        putOnPage(data.data)
-      })
-  }
-
-  // GET /api/favorites Retrieve all favorites
-  const getFavoirtes = () => {
-    return axios.get('/favorites')
-      .then(data => {
-        putOnPage(data.data)
-      })
-  }
-=======
->>>>>>> 75d57549ff9e6c350f70c3010b1a4b63da95ec17
