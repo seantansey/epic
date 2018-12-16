c92ef7bff70cb9169b1f4b1cf03f29355d486c82
Paul Mourer <paul.mourer@gmail.com>
2018-12-16 14:12:12 -0700

    go to login page button now takes you to login

--- public/javascripts/signUp.js
+++ public/javascripts/signUp.js
@@ -1,13 +1,20 @@
-document.addEventListener('DOMContentLoaded',() => {
-const loginForm = document.getElementById("Login")
-loginForm.addEventListener("submit",(event)=> {
-event.preventDefault()
-console.log(loginForm.inputEmail.value)
-let user= {}
-user.email= loginForm.inputEmail.value
-user.password=loginForm.inputPassword.value
-createNewUser(user).then(data => {
-  console.log(data)
-})
-})
+document.addEventListener('DOMContentLoaded', () => {
+  const loginForm = document.getElementById("Login")
+  loginForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    console.log(loginForm.inputEmail.value)
+    let user = {}
+    user.email = loginForm.inputEmail.value
+    user.password = loginForm.inputPassword.value
+    createNewUser(user).then(data => {
+      console.log(data)
+    })
+  })
+
+  const goToLoginBtn = document.getElementById('login-page-btn')
+
+  goToLoginBtn.addEventListener('click', () => {
+    console.log('hello')
+    window.location.href='HomePage.html'
+  })
 })
