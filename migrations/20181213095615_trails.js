f1bea694a7b716dcb87652c6918dbbbab2b5218a
Sean Tansey <sean.d.tansey@gmail.com>
2018-12-16 13:30:46 -0700

    heroku5

--- migrations/20181213095615_trails.js
+++ migrations/20181213095615_trails.js
@@ -1,29 +1,25 @@
 
 exports.up = function(knex) {
-  return knex.schema.createTable('trails', table => {
-    table.increments()
-    table.string('name').notNullable()
-    table.string('difficulty').notNullable()
-    table.string('summary').notNullable()
-    table.float('stars')
-    table.string('url')
-    table.string('location').notNullable()
-    table.integer('mtbproject_id')
-    table.string('img_medium')
-    table.float('length')
-    table.integer('ascent')
-    table.integer('descent')
-    table.integer('high_point')
-    table.integer('low_point')
-    table.integer('longitude')
-    table.integer('latitude')
-    table.integer('high')
-    table.integer('low')
-    table.float('longitude')
-    table.float('latitude')
-  })
+ return knex.schema.createTable('trails', table => {
+   table.increments()
+   table.string('name').notNullable()
+   table.string('difficulty').notNullable()
+   table.string('summary').notNullable()
+   table.float('stars')
+   table.string('url')
+   table.string('location').notNullable()
+   table.integer('mtbproject_id')
+   table.string('img_medium')
+   table.float('length')
+   table.integer('ascent')
+   table.integer('descent')
+   table.integer('high')
+   table.integer('low')
+   table.float('longitude')
+   table.float('latitude')
+ })
 }
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('trails')
+ return knex.schema.dropTable('trails')
 }
